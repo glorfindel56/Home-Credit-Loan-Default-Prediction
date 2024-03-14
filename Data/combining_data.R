e3b7503c31aaf0ce829611680390b86a6b71af3a
Joyce Hu <jhu465@gatech.edu>
2024-03-13 22:05:48 -0700

    converting column names to lower case for each dataset

--- Data/combining_data.R
+++ Data/combining_data.R
@@ -22,7 +22,19 @@ pos_cash_balance <- read_csv("/Users/joycehu/Library/CloudStorage/Box-Box/MGT 62
 previous_application <- read_csv("/Users/joycehu/Library/CloudStorage/Box-Box/MGT 6203/previous_application.csv",
                                  col_names = TRUE)
 
+# converting column names to lower case for each dataset
+colnames(application) <- str_to_lower(colnames(application))
+colnames(bureau) <- str_to_lower(colnames(bureau))
+colnames(bureau_balance) <- str_to_lower(colnames(bureau_balance))
+colnames(credit_card_balance) <- str_to_lower(colnames(credit_card_balance))
+colnames(installments_payments) <- str_to_lower(colnames(installments_payments))
+colnames(pos_cash_balance) <- str_to_lower(colnames(pos_cash_balance))
+colnames(previous_application) <- str_to_lower(colnames(previous_application))
+
+# combining into one large dataset using common foreign keys on a few policies
+head(application)
+
 # viewing the data to do a simple sanity check
 head(application)
 
-
+str_to_lower(colnames(application))
