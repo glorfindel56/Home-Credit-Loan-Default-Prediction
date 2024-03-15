1cbd3f22cb4e9fcd0ebdb783500a5fdf4040133e
Joyce Hu <jhu465@gatech.edu>
2024-03-14 21:37:57 -0700

    combining all datasets related to Home Credit loans

--- Data/combining_data.R
+++ Data/combining_data.R
@@ -31,10 +31,112 @@ colnames(installments_payments) <- str_to_lower(colnames(installments_payments))
 colnames(pos_cash_balance) <- str_to_lower(colnames(pos_cash_balance))
 colnames(previous_application) <- str_to_lower(colnames(previous_application))
 
+# renaming columns by appending dataset initials to the end of each column in each dataset
+application <- application %>% rename_with(~ paste0(., "_a"), everything())
+bureau <- bureau %>% rename_with(~ paste0(., "_b"), everything())
+bureau_balance <- bureau_balance %>% rename_with(~ paste0(., "_bb"), everything())
+credit_card_balance <- credit_card_balance %>% rename_with(~ paste0(., "_cbb"), everything())
+installments_payments <- installments_payments %>% rename_with(~ paste0(., "_ip"), everything())
+pos_cash_balance <- pos_cash_balance %>% rename_with(~ paste0(., "_pcb"), everything())
+previous_application <- previous_application %>% rename_with(~ paste0(., "_pa"), everything())
+
 # combining into one large dataset using common foreign keys on a few policies
-head(application)
 
-# viewing the data to do a simple sanity check
-head(application)
+############## combining Home Credit data on one policy number ##############
+# does not include bureau data
+# policy number = 100002
 
-str_to_lower(colnames(application))
+# joining application and previous_application
+combined_small <- application %>%
+  filter(sk_id_curr_a == 100002) %>%
+  left_join(., previous_application, by = c("sk_id_curr_a" = "sk_id_curr_pa")) 
+
+# joining installments_payments to combined_small
+# there is a 1-to-N relationship between combined_small and installments_payments
+# ex: sk_id_curr_a == 100002 has a 1-to-19 relationship (19 records in installments_payments)
+# grabbing the latest information from installments_payments and merging it onto combined_small
+# this is the best gauge on the customer's recent financial payments history
+# also results in a 1-to-1 relationship between combined_small and installments_payments
+i <- installments_payments %>% filter(sk_id_curr_ip == 100002)
+
+installments_payments_2 <- installments_payments %>%
+                           group_by(sk_id_curr_ip, sk_id_prev_ip) %>%
+                           filter(num_instalment_version_ip == max(num_instalment_version_ip),
+                                  num_instalment_number_ip == max(num_instalment_number_ip), 
+                                  days_instalment_ip == max(days_instalment_ip),
+                                  days_entry_payment_ip == max(days_entry_payment_ip),
+                                  amt_payment_ip == max(amt_payment_ip))
+
+combined_small_2 <- combined_small %>%
+  left_join(., installments_payments_2, by = c("sk_id_prev_pa" = "sk_id_prev_ip"))
+
+# joining pos_cash_balance to combined_small_2
+# there is also a 1-to-N relationship between combined_small_2 and pos_cash_balance
+# going to follow the same logic as above to get a 1-to-1 relationship
+p <- pos_cash_balance %>% 
+  filter(sk_id_curr_pcb == 100002)
+
+pos_cash_balance_2 <- pos_cash_balance %>%
+                      group_by(sk_id_curr_pcb, sk_id_prev_pcb) %>%
+                      filter(months_balance_pcb == max(months_balance_pcb))
+
+combined_small_3 <- combined_small_2 %>%
+  left_join(., pos_cash_balance_2, by = c("sk_id_prev_pa" = "sk_id_prev_pcb"))
+
+# joining credit_card to combined_small_3
+# going to follow the same logic as above to get a 1-to-1 relationship
+cc <- credit_card_balance %>%
+  filter(sk_id_curr_cbb == 100002)
+
+credit_card_balance_2 <- credit_card_balance %>%
+  group_by(sk_id_curr_cbb, sk_id_prev_cbb) %>%
+  filter(months_balance_cbb == max(months_balance_cbb))
+
+combined_small_4 <- combined_small_3 %>%
+  left_join(., credit_card_balance, by = c("sk_id_prev_pa" = "sk_id_prev_cbb"))
+
+
+############## combining Home Credit data on ALL policy numbers ##############
+combined <- application %>%
+  left_join(., previous_application, by = c("sk_id_curr_a" = "sk_id_curr_pa")) %>%
+  arrange(sk_id_curr_a)
+
+combined_2 <- combined %>%
+  left_join(., installments_payments_2, by = c("sk_id_prev_pa" = "sk_id_prev_ip")) %>%
+  arrange(sk_id_curr_a)
+
+combined_3 <- combined_2 %>%
+  left_join(., pos_cash_balance_2, by = c("sk_id_prev_pa" = "sk_id_prev_pcb"))
+
+combined_4 <- combined_3 %>%
+  left_join(., credit_card_balance_2, by = c("sk_id_prev_pa" = "sk_id_prev_cbb"))
+
+# debug why combined_2 doesn't have the same amount of rows as combined
+# need to do data manipuldation on installments_payments
+a <- combined %>% filter(sk_id_curr_a == 100213)
+b <- combined_2 %>% filter(sk_id_curr_a == 100213) %>% distinct()
+c <- installments_payments_2 %>% filter(sk_id_curr_ip == 100213)
+
+d <- combined %>% group_by(sk_id_curr_a) %>% summarize(count = n())
+e <- combined_2 %>% group_by(sk_id_curr_a) %>% summarize(count = n())
+f <- d %>%
+  left_join(., e, by = c("sk_id_curr_a"), suffix = c("c","c2")) %>%
+  mutate(diff = countc2 - countc)
+
+# debug why combined_4 doesn't have the same amount of rows as combined_3
+# need to do data manipulation on credit_card_balance
+a <- combined_3 %>% filter(sk_id_curr_a == 317063)
+b <- combined_4 %>% filter(sk_id_curr_a == 317063)
+c <- credit_card_balance %>% filter(sk_id_curr_cbb == 317063)
+
+d <- combined_3 %>% group_by(sk_id_curr_a) %>% summarize(count = n())
+e <- combined_4 %>% group_by(sk_id_curr_a) %>% summarize(count = n())
+f <- d %>%
+  left_join(., e, by = c("sk_id_curr_a"), suffix = c("c","c2")) %>%
+  mutate(diff = countc2 - countc)
+
+
+
+
+# joining bureau to combined_small_4
+# going to follow the same logic as above to get a 1-to-1 relationship
