4c5a9eaf4f5d7e01acf26d2b41a70bf6fa40b2e6
Joyce Hu <jhu465@gatech.edu>
2024-03-16 14:15:32 -0700

    exploratory analysis for categorical variables

--- Code/cleaning_data.R
+++ Code/cleaning_data.R
@@ -123,22 +123,24 @@ application <- application %>% rename_with(~ paste0(., "_a"), everything())
 
 # count of defaulters vs non-defaulters for categorical variables in a graph and table
 combined <- function(data, variable){
-  variable <- enquo(variable)
   
   graph <- data %>%
     mutate(target_a = factor(target_a)) %>%
-    group_by(!!variable, target_a) %>%
+    group_by(!!sym(variable), target_a) %>%
     summarize(n = n(), .groups = "drop") %>% # suppress warning message
-    ggplot(aes(x = !!variable, y = n, fill = target_a)) +
-    geom_bar(stat = "identity") +
+    mutate(!!sym(variable) := factor(!!sym(variable))) %>%
+    ggplot(aes(x = !!sym(variable), y = n, fill = target_a)) +
+    geom_bar(stat = "identity", na.rm = TRUE) +
     labs(y = "Count") +
     theme(axis.text.x = element_text(angle = 45, hjust = 1))
   
   table <- data %>%
-    group_by(!!variable, target_a) %>%
+    group_by(!!sym(variable), target_a) %>%
     summarize(n = n(), .groups = "drop") %>%
+    # mutate(n = if_else(is.na(.), 0, .)) %>%
     pivot_wider(., names_from = target_a, values_from = n) %>%
-    mutate(across(everything(), ~ifelse(is.na(.), 0, .))) %>%
+    # mutate(across(everything(), ~ifelse(is.na(.), 0, .))) %>%
+    # mutate(!!variable := if_else(!!variable == 0, "NA", !!variable)) %>%
     mutate(default_rate = round(.[ , c(3)] / rowSums(.[ , c(2, 3)]), 2))
   
   table <- tableGrob(table)
@@ -146,17 +148,61 @@ combined <- function(data, variable){
   combined <- grid.arrange(table, graph, nrow = 2)
 }
 
-
 # categorical variables: exploratory analysis
-contract_type <- combined(application, name_contract_type_a) # mostly cash loans, higher default rate w cash loans
-gender <- combined(application, code_gender_a) # women take out much more number of loans than men
+contract_type <- combined(application, "name_contract_type_a") # mostly cash loans, higher default rate w cash loans
+gender <- combined(application, "code_gender_a") # women take out much more number of loans than men
 # women have a lower default rate too
-own_car <- combined(application, flag_own_car_a) # most clients do not own a car
-own_real_estate <- combined(application, flag_own_realty_a) # most clients own a home
-# interestingly, those that own real estate have a higher default rate
+own_car <- combined(application, "flag_own_car_a") # most clients do not own a car
+own_real_estate <- combined(application, "flag_own_realty_a") # most clients own a home
+# interestingly, the default rate is about the same for those that own vs do not own a home (not intuitive)
+num_of_children <- combined(application, "cnt_children_capped_a") # those with no children have a lower default rate
+name_type <- combined(application, "name_type_suite_a") # not very helpful visual, this variable can be dropped
+income_type <- combined(application, "name_income_type_a")
+education_type <- combined(application, "name_education_type_a")
+family_status <- combined(application, "name_family_status_a")
+housing_type <- combined(application, "name_housing_type_a") # not very helpful visual, most are categorized under
+# House / apartment - this variable can be dropped
+own_car_age <- combined(application, "own_car_age_a") # most entries are NAs, drop the var
+mobile_flag <- combined(application, "flag_mobil_a") # most entries are in the 1 group, drop the var
+
+# creating a list of categorical variables that need to be inspected w exploratory analysis
+cat_var <- c("flag_emp_phone_a", "flag_work_phone_a", "flag_cont_mobile_a", 
+             "flag_phone_a", "flag_email_a", "occupation_type_a",
+             "cnt_fam_members_a", "region_rating_client_a", "region_rating_client_w_city_a",
+             "weekday_appr_process_start_a", "hour_appr_process_start_a", "reg_region_not_live_region_a",
+             "reg_region_not_work_region_a", "live_region_not_work_region_a", "reg_city_not_live_city_a",
+             "reg_city_not_work_city_a", "live_city_not_work_city_a", "organization_type_a",
+             "fondkapremont_mode_a", "housetype_mode_a", "wallsmaterial_mode_a", 
+             "obs_30_cnt_social_circle_a", "def_30_cnt_social_circle_a", "obs_60_cnt_social_circle_a",
+             "def_60_cnt_social_circle_a", "flag_document_2_a", "flag_document_3_a", "flag_document_4_a",
+             "flag_document_5_a", "flag_document_6_a", "flag_document_7_a",
+             "flag_document_8_a", "flag_document_9_a", "flag_document_10_a",            
+             "flag_document_11_a", "flag_document_12_a", "flag_document_13_a",           
+             "flag_document_14_a", "flag_document_15_a", "flag_document_16_a",            
+             "flag_document_17_a", "flag_document_18_a", "flag_document_19_a",           
+             "flag_document_20_a", "flag_document_21_a", "amt_req_credit_bureau_hour_a",  
+             "amt_req_credit_bureau_day_a", "amt_req_credit_bureau_week_a", "amt_req_credit_bureau_mon_a",   
+             "amt_req_credit_bureau_qrt_a", "amt_req_credit_bureau_year_a")
+
+for (i in cat_var) {
+  combined(application, {{i}})
+}
+
+# results based on looking at the plots created for the categorical variables
+# variables that can be dropped based on high correlation to other variables:
+
+
+# variables that need min/max capping: amt_req_credit_bureau_year_a (cap at 9),
+# amt_req_credit_bureau_qrt_a (cap at 4), amt_req_credit_bureau_mon_a (cap at 12),
+# amt_req_credit_bureau_week_a (cap at 2), amt_req_credit_bureau_day_a (cap at 1),
+# amt_req_credit_bureau_hour_a (cap at 1), def_60_cnt_social_circle_a (cap at 2),
+# obs_60_cnt_social_circle_a (cap at 5), 
+
+# variables to drop based on exploratory analysis
+application_vars_drop <- c("name_type_suite_a", "housing_type", "own_car_age_a",
+                           "flag_mobil_a")
 
 x <- head(application)
-
 
 # graph <- function(variable){
 #   home_credit_2 %>%
@@ -173,8 +219,11 @@ x <- head(application)
 
 
 
-######### binning and cleaning variables #########
+######### binning, capping, and transforming variables #########
+application <- application %>%
+  mutate(cnt_children_capped_a = if_else(cnt_children_a >=5, "5+", as.factor(cnt_children_a)))
 
+# need to transform days_birth_a, days_employed_a, days_registration_a, days_id_publish_a
 
 
 
